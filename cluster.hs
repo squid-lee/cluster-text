603ef8cea2e513c96c341f171c4a8f303b9dc70f
Joseph Slade <slade118118+github@gmail.com>
2016-05-18 17:44:35 +0100

    Monomorphic over bytestrings

--- cluster.hs
+++ cluster.hs
@@ -4,17 +4,20 @@ import Data.Ord
 import Data.List (minimumBy)
 import System.Environment (getArgs)
 
-naiveCluster :: Int -> [C.ByteString] -> [(C.ByteString, [C.ByteString])]
-naiveCluster maxDist xs = M.toList $ go xs M.empty
+naiveCluster :: Int -> (C.ByteString -> C.ByteString -> Int) -> [C.ByteString] -> [(C.ByteString, [C.ByteString])]
+naiveCluster maxDist dist xs = M.toList $ go xs M.empty
   where
     go :: [C.ByteString] -> Clusters C.ByteString -> Clusters C.ByteString
     go [] cs = cs
     go (x:xs) cs
-      | dist3 x best <= maxDist = go xs (M.insert best (x : bestValues) cs)
+      | dist x nearest <= maxDist = go xs (M.insert nearest (x : nearestNeighs) cs)
       | otherwise = go xs (M.insert x [] cs)
       where
-        best = bestCluster x dist3 cs
-        bestValues = maybe [] id (M.lookup best cs)
+        nearest :: C.ByteString
+        nearest = bestCluster x dist cs
+
+        nearestNeighs :: [C.ByteString]
+        nearestNeighs = maybe [] id (M.lookup nearest cs)
 
 
 dist2 x y = go (C.words x) (C.words y)
@@ -31,7 +34,7 @@ dist3 x y = dist2 (C.drop 87 x) (C.drop 87 y)
 
 type Clusters a = M.Map a [a]
 
-bestCluster :: (Ord dist, Num dist) => a -> (a -> a -> dist) -> Clusters a -> a
+bestCluster :: a -> (a -> a -> Int) -> Clusters a -> a
 bestCluster candidate distanceFrom clusters
  | M.null clusters = candidate
  | otherwise = minimumBy (comparing (distanceFrom candidate)) (M.keys clusters)
@@ -41,7 +44,7 @@ main = do
   (file:maxDist':_) <- getArgs
   let maxDist = read maxDist'
   fileContents <- C.readFile file
-  mapM_ (C.putStrLn . pp) . (naiveCluster maxDist)  . C.lines $ fileContents
+  mapM_ (C.putStrLn . pp) . (naiveCluster maxDist dist3)  . C.lines $ fileContents
 
 pp :: (C.ByteString, [C.ByteString]) -> C.ByteString
 pp (cand, neighs) = C.unlines $ (C.concat [cand, (C.pack " "), (C.pack . show $ length neighs)]) :  (map (\x -> C.append (C.pack "  ") x) neighs)
