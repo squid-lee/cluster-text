45915a048949fa2b61a9e90c2f1adc9d1df44d2e
Joseph Slade <slade118118+github@gmail.com>
2016-05-23 14:07:06 +0100

    Added (buggy) entropy measure for clusters

--- cluster.hs
+++ cluster.hs
@@ -59,3 +59,38 @@ main = do
 
 pp :: (C.ByteString, [C.ByteString]) -> C.ByteString
 pp (cand, neighs) = C.unlines $ (C.concat [cand, (C.pack " "), (C.pack . show $ length neighs)]) :  (map (\x -> C.append (C.pack "  ") x) neighs)
+
+ppEntropy :: (C.ByteString, [C.ByteString]) -> C.ByteString
+ppEntropy (cand, neighs) = C.unlines $ map C.unwords [words', entropies']
+  where
+    (words, entropies) = unzip $ clusterEntropy (cand : neighs)
+    (words', entropies') = unzip $ zipWith padToEqual words entropies
+
+    -- Pad x's && y's representation to be the same length
+    padToEqual :: C.ByteString -> Double -> (C.ByteString, C.ByteString)
+    padToEqual x y = (C.append x (C.replicate (-1*difference) ' '), C.append y' (C.replicate difference ' '))
+      where
+        y' = C.pack . take 5 . show $ y
+        difference = C.length x - C.length y'
+
+-- xs is a list of log lines
+clusterEntropy :: [C.ByteString] -> [(C.ByteString, Double)]
+clusterEntropy xs = map (head &&& listEntropy) asWords
+  where
+    asWords = transpose $ map C.words xs
+
+listEntropy :: (Eq a) => [a] -> Double
+listEntropy xs = let (_, _, e) = listEntropy' xs in e
+
+listEntropy' :: (Eq a) => [a] -> ([Double], Double, Double)
+listEntropy' [] = ([], 0, 0)
+listEntropy' xs = (map px xs, qx, entropy)
+  where
+    entropy = sum $ map singleEntropy xs
+    singleEntropy x = ((px x) * log2 ((px x) / qx))
+
+    px x = (count x xs) / genericLength xs
+    qx = 1 / genericLength (nub xs) -- if xs were uniformly distributed
+
+    log2 n = log n / log 2
+    count x xs = genericLength $ filter (==x) xs
